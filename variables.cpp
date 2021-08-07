6d7eaccf4fc40dcfca1ba1f09d44304764b604f8
SHRUTI BANERJEE <86586883+ShrutiBanerjee03@users.noreply.github.com>
2021-08-07 12:43:26 +0530

    variables basic program

---
+++ variables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int myNum = 15;
+cout << myNum;
+const int minutesPerHour = 60;
+const float PI = 3.14;
+//user input
+int x; 
+cout << "\n Type a number: "; // Type a number and press enter
+cin >> x; // Get user input from the keyboard
+cout << "\n Your number is: " << x; // Display the input value
+}
